b9fde446d7a5a9323db69b405a3838b4df53d59b
eliwilliamson <eliwilliamson@gmail.com>
2018-05-24 16:28:49 -0400

    first pass

---
+++ variables.js
@@ -0,0 +1,45 @@
+// if you change these you must restart the server
+
+module.exports = {
+
+  // colors
+  offWhite: '#fbfbfb',
+  lightestGrey: '#E6E6E6',
+  lighterGrey: '#BCBCBC',
+  lightGrey: '#7D8E97',
+  grey: '#133344',
+  blue: '#24A1D8',
+  yellow: '#FEDC00',
+
+  // typography
+  light: 300,
+  regular: 400,
+  bold: 700,
+
+  // fonts
+  spectral: "'Spectral', serif",
+  roboto: "'Roboto', sans-serif",
+
+  // padding
+  micro: '8px',
+  tiny: '16px',
+  small: '24px',
+  medium: '40px',
+  large: '64px',
+  xl: '104px',
+  xxl: '168px',
+
+  // border radius
+  smallBorderRadius: '4px',
+  borderRadius: '10px',
+
+  // responsive breakpoints
+  mobileMax: '479px',
+  mobile: '480px',
+  tabletMax: '767px',
+  tablet: '768px',
+  desktopMax: '959px',
+  desktop: '960px',
+  displayMax: '1199px',
+  display: '1200px',
+}
