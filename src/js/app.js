7353292134bfc886612020e631aaeaa17d5b23fb
eliwilliamson <eliwilliamson@gmail.com>
2018-05-25 09:49:54 -0400

    add netlify form

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,31 @@
-var theForm = document.getElementById( 'theForm' );
+var theForm = document.getElementById( 'contact' );
 
 new stepsForm( theForm, {
   onSubmit : function( form ) {
-    // hide form
     classie.addClass( theForm.querySelector( '.simform-inner' ), 'hide' );
 
-    /*
-    form.submit()
-    or
-    AJAX request (maybe show loading indicator while we don't have an answer..)
-    */
-
-    // let's just simulate something...
-    var messageEl = theForm.querySelector( '.final-message' );
-    messageEl.innerHTML = 'Thank you! We\'ll be in touch.';
-    classie.addClass( messageEl, 'show' );
+    // form.submit()
+    postAjax(form.getAttribute("action"), serialize(document.forms[0]), function(){
+      var messageEl = theForm.querySelector( '.final-message' );
+      messageEl.innerHTML = 'Thank you! We\'ll be in touch.';
+      classie.addClass( messageEl, 'show' );
+    });
   }
 } );
+
+
+function postAjax(url, data, success) {
+  var params = typeof data == 'string' ? data : Object.keys(data).map(
+          function(k){ return encodeURIComponent(k) + '=' + encodeURIComponent(data[k]) }
+      ).join('&');
+
+  var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+  xhr.open('POST', url);
+  xhr.onreadystatechange = function() {
+      if (xhr.readyState>3 && xhr.status==200) { success(xhr.responseText); }
+  };
+  xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  xhr.send(params);
+  return xhr;
+}
